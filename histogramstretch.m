bd4c9426d87c892d2e58d4e90aed030aa19e8e38
hangyav <hangyav@ecf88ebb-0997-779f-c167-1ac3aaabe8c3>
2012-09-30 14:28:06 +0000

    hiszogram szamitas, megjelenites es szethuzas, plusz egy bw kep

---
+++ histogramstretch.m
@@ -0,0 +1,23 @@
+function res = histogramstretch(im, max_value = 255)
+	[n, m, t] = size(im);
+	res = [];
+	for layer = [1:t]
+		h = histogram(im, layer, max_value+1);
+		minv = 0;
+		maxv = max_value;
+		for a = [1:size(h,2)]
+			if h(a) ~= 0
+				maxv = a;
+				if minv == 0
+					minv = a;
+				end
+			end
+		end
+		tmp = ((im(:,:,layer) - minv).*(max_value/maxv));
+		if size(res, 1) == 0
+			res = tmp;
+		else
+			res = cat(3, res, tmp);
+		end
+	end
+end
